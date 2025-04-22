6841b7c501a54368d52ac9b31e801208afed4a6a
navneetrai2111 <navneet.r@zeksta.com>
2025-04-22 23:09:21 +0530

    ch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <HeaderTop />
+        
         <HeaderMin />
         <Navbar />
         {children}
